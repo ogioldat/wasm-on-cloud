3e6689d57ef49eeb0750815773654e79fbb7846b
ogioldat <tomasz.ogiolda00@gmail.com>
2023-11-25 18:44:48 +0100

    feat: menu-api

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 use spin_sdk::http::{IntoResponse, Request};
 use spin_sdk::http_component;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Clone)]
+struct MenuItem {
+    name: String,
+    price: f32,
+}
+
+fn get_menu_items(): Vec<MenuItem> {
+
+}
 
 /// A simple Spin HTTP component.
 #[http_component]
-fn handle_cloud_function(req: Request) -> anyhow::Result<impl IntoResponse> {
+fn handle_restaurant(req: Request) -> anyhow::Result<impl IntoResponse> {
     println!("Handling request to {:?}", req.header("spin-full-url"));
     Ok(http::Response::builder()
         .status(200)
